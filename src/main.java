895109591503951a78306395a25cd1403f20a2db
matttse <tsemd@bu.edu>
2017-03-15 13:18:45 -0400

    move file

--- src/main.java
+++ src/main.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * 
  */
@@ -22,6 +27,22 @@ public class main {
 		regexInput myVal = new regexInput();
 		System.out.println(myVal.getInputValue(clippy));//pass clipboard to object
 		
+		try {
+			File file = new File("c:/location.txt");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				stringBuffer.append(line);
+				stringBuffer.append("\n");
+			}
+			fileReader.close();
+			System.out.println("Contents of file:");
+			System.out.println(stringBuffer.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
--- src/regexInput.java
+++ src/regexInput.java
@@ -22,25 +22,7 @@ public class regexInput {
 	 * @param args
 	 * @return 
 	 */
-	public String getInputValue(String clippy) {
-		
-		try {
-			File file = new File("C:/Users/ch165496/Desktop/serial12345666666.txt");
-			FileReader fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			StringBuffer stringBuffer = new StringBuffer();
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				stringBuffer.append(line);
-				stringBuffer.append("\n");
-			}
-			fileReader.close();
-			System.out.println("Contents of file:");
-			System.out.println(stringBuffer.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+	public String getInputValue(String clippy) {		
 		
 		// Declare Pattern
 		pattern = "\\[(.)(.)\\]";
