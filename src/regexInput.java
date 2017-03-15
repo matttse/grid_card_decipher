b24bcceb1d253c366fdfb7759193d69323e6904d
matttse <tsemd@bu.edu>
2017-03-15 13:12:55 -0400

    updates
    gets clipboard
    gets pattern
    gets file

--- src/regexInput.java
+++ src/regexInput.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.*;
 
 /**
  * @author MTse
  *
  */
-public class regexInput extends valuePair {
+public class regexInput {
 	
 //	protected String[] inputValue;
 	List<String> inputValue = new ArrayList<String>();
@@ -22,8 +23,27 @@ public class regexInput extends valuePair {
 	 * @return 
 	 */
 	public String getInputValue(String clippy) {
+		
+		try {
+			File file = new File("C:/Users/ch165496/Desktop/serial12345666666.txt");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				stringBuffer.append(line);
+				stringBuffer.append("\n");
+			}
+			fileReader.close();
+			System.out.println("Contents of file:");
+			System.out.println(stringBuffer.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
 		// Declare Pattern
-		pattern = "\\[..\\]";//"\[.?)\]";
+		pattern = "\\[(.)(.)\\]";
 		
 		// pattern object
 		Pattern checking = Pattern.compile(pattern);
@@ -41,4 +61,7 @@ public class regexInput extends valuePair {
 		return inputValue.toString();
 		
 	}
+	
+
+	
 }
